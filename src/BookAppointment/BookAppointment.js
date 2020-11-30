73b7f56b632879e48400f2331a7b95a3c17d356e
shassaan <syedhassaan.103@gmail.com>
2020-11-30 18:10:14 +0500

    bookappointment

--- src/BookAppointment/BookAppointment.js
+++ src/BookAppointment/BookAppointment.js
@@ -1,76 +1,79 @@
 import React from 'react'
-import { Card, Col, Form, Row,Alert } from 'react-bootstrap';
+import { Card, Col, Form, Row, Alert } from 'react-bootstrap';
 const BookAppointment = () => {
     return (<>
-        <Col xs={"10"}>
             <div>
-                <Card style={{border:"none"}}>
+                <Card style={{ border: "none" }}>
                     <Card.Body>
                         <h2 className="text text-muted">Appointment type</h2>
                         <Row>
                             <Col md="4">
-                               <Alert variant="success">
-                               <Form.Check
-                                    style={{ padding: "1em", border: "1px dotted greenyellow" }}
-                                    type="radio"
-                                    label={`Appointment Type 1`}
+                                <Alert variant="success">
+                                    <Form.Check
+                                    
+                                        style={{ padding: "1em", border: "1px dotted greenyellow" ,cursor:"pointer"}}
+                                        type="radio"
+                                        label={`Appointment Type 1`}
+                                        name="appointmenttype"
 
-                                />
-                               </Alert>
+                                    />
+                                </Alert>
 
                             </Col>
                             <Col md="4">
                                 <Alert variant="success">
-                                <Form.Check
-                                    style={{ padding: "1em", border: "1px dotted greenyellow" }}
-                                    type="radio"
-                                    label={`Appointment Type 2`}
-
-                                />
+                                    <Form.Check
+                                    className="selectable"
+                                        style={{ padding: "1em", border: "1px dotted greenyellow" ,cursor:"pointer"}}
+                                        type="radio"
+                                        label={`Appointment Type 2`}
+                                        name="appointmenttype"
+                                    />
                                 </Alert>
 
                             </Col>
                             <Col md="4">
-                                <Alert  variant="success" >
-                                <Form.Check
-                                    style={{ padding: "1em", border: "1px dotted greenyellow" }}
-                                    type="radio"
-                                    label={`Appointment Type 3`}
-
-                                />
+                                <Alert variant="success" >
+                                    <Form.Check
+                                    className="selectable"
+                                        style={{ padding: "1em", border: "1px dotted greenyellow" ,cursor:"pointer"}}
+                                        type="radio"
+                                        label={`Appointment Type 3`}
+                                    name="appointmenttype"
+                                    />
                                 </Alert>
 
                             </Col>
                         </Row>
-                        <hr/>                        <h2 className="text text-muted">Datetime</h2>
+                        <hr />
+                        <h2 className="text text-muted">Datetime</h2>
                         <Row>
                             <Col md={"12"}><Form.Control type="date" /></Col>
                         </Row>
-                        <hr/>    
+                        <hr />
                         <h2 className="text text-muted">Payment details</h2>
                         <Row>
                             <Col md={"6"}>
-                                <Form.Control type="text" placeholder="Card no." /> <br/>
-                            </Col>
-                            
-                            <Col md={"4"}>
-                                <Form.Control type="date" placeholder="Expiry" /> <br/>
+                                <Form.Control type="text" placeholder="Card no." /> <br />
                             </Col>
 
-                            <br/>
+                            <Col md={"4"}>
+                                <Form.Control type="date" placeholder="Expiry" /> <br />
+                            </Col>
+
+                            <br />
 
                             <Col md={"2"}>
-                                <Form.Control type="text" placeholder="CVV" /> <br/> <br/>
+                                <Form.Control type="text" placeholder="CVV" /> <br /> <br />
                             </Col>
                         </Row>
-                        
+
                         <Row>
                             <Col><button className="btn btn-success">Book now</button></Col>
                         </Row>
                     </Card.Body>
                 </Card>
             </div>
-        </Col>
     </>);
 }
 
