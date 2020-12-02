873faa6864ece624706785587e596535dc093804
shassaan <syedhassaan.103@gmail.com>
2020-12-02 15:37:04 +0500

    added appointment

--- src/BookAppointment/BookAppointment.js
+++ src/BookAppointment/BookAppointment.js
@@ -1,45 +1,49 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Card, Col, Form, Row, Alert } from 'react-bootstrap';
 const BookAppointment = () => {
+    const [checked,SetIsChecked] = useState({app1 : false,app2 : false , app3 : false})
     return (<>
             <div>
-                <Card style={{ border: "none" }}>
+                <Card style={{ border: "none" ,cursor:"pointer"}} >
                     <Card.Body>
                         <h2 className="text text-muted">Appointment type</h2>
                         <Row>
                             <Col md="4">
-                                <Alert variant="success">
+                                <Alert variant="success" onClick={()=>{SetIsChecked({app1 : true,app2 : false , app3 : false})}}>
                                     <Form.Check
                                     
-                                        style={{ padding: "1em", border: "1px dotted greenyellow" ,cursor:"pointer"}}
+                                        style={{ padding: "1em", border: "1px dotted greenyellow"}}
                                         type="radio"
                                         label={`Appointment Type 1`}
+                                        checked = {checked.app1}
                                         name="appointmenttype"
-                                        onClick={e => console.log("Appointment 1 selected")}
+                                        
                                     />
                                 </Alert>
 
                             </Col>
                             <Col md="4">
-                                <Alert variant="success">
+                                <Alert variant="success"  onClick={()=>{SetIsChecked({app1 : false,app2 : true , app3 : false})}}>
                                     <Form.Check
                                     className="selectable"
                                         style={{ padding: "1em", border: "1px dotted greenyellow" ,cursor:"pointer"}}
                                         type="radio"
                                         label={`Appointment Type 2`}
                                         name="appointmenttype"
-                                        onClick={e => console.log("Appointment 2 selected")}
+                                        checked = {checked.app2}
+                                       
                                     />
                                 </Alert>
 
                             </Col>
                             <Col md="4">
-                                <Alert variant="success" >
+                                <Alert variant="success"  onClick={()=>{SetIsChecked({app1 : false,app2 : false , app3 : true})}}>
                                     <Form.Check
                                     className="selectable"
                                         style={{ padding: "1em", border: "1px dotted greenyellow" ,cursor:"pointer"}}
                                         type="radio"
                                         label={`Appointment Type 3`}
+                                        checked = {checked.app3}
                                         onClick={e => console.log("Appointment 3 selected")}
                                     name="appointmenttype"
                                     />
