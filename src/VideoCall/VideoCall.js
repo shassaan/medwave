9e5eb96488d6052aacd9ba9ba0b3434ca12670c5
syed hassaan <hassan.ahmed@eonhealth.com>
2020-12-11 15:48:25 +0500

    loader

--- src/VideoCall/VideoCall.js
+++ src/VideoCall/VideoCall.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Col } from 'react-bootstrap'
 const VideoCall = ()=>{
+  const [poster,setPoster] = useState(false)
 return (<>
 <div className="container">
-<video  preload="auto" style={{width:"70%",position:"absolute"}}>
+<video  preload="auto" style={{width:"70%",position:"absolute"}} poster = {poster ? `${process.env.PUBLIC_URL}/loader.gif`:""}>
   <source src={`${process.env.PUBLIC_URL}/video.mp4`} type="video/mp4"/>
 </video>
 
-<video  preload="auto" style={{position:"relative",height:"60px",border:"1px solid white"}}>
+<video  preload="auto" style={{position:"relative",height:"60px",border:"1px solid white"}} poster = {poster ? `${process.env.PUBLIC_URL}/loader.gif`:""}>
   <source src={`${process.env.PUBLIC_URL}/video.mp4`} type="video/mp4"/>
 </video>
 </div>
+
+<button onClick={()=>setPoster(!poster)}>toggle loader</button>
 </>)
 }
 
